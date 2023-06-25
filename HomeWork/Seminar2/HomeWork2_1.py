da2755ebf4302347a9cf7130707b7cfde1f78f06
AndreyZobnin <111076368+AndreyZobnin@users.noreply.github.com>
2023-06-25 21:45:15 +0300

    Add files via upload

---
+++ HomeWork/Seminar2/HomeWork2_1.py
@@ -0,0 +1,25 @@
+
+
+# Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
+# Функцию hex используйте для проверки своего результата.
+
+def convert_num(num: int, mode: str) -> str:
+    result = ''
+    convert: int = 1
+    match mode:
+        case "bin":
+            convert = 2
+        case "oct":
+            convert = 8
+        case "hex":
+            convert = 16
+    while num >= 1:
+        res = num % convert
+        result += str(res)
+        num = num // convert
+    return result[::-1]
+
+
+print(convert_num(88, mode="bin"), f"assert: {bin(88)}")
+print(convert_num(88, mode="oct"), f"assert: {oct(88)}")
+print(convert_num(88, mode="hex"), f"assert: {hex(88)}")

ab4ae694b40e64ec0a37c692fd38086d257459eb
AndreyZobnin <111076368+AndreyZobnin@users.noreply.github.com>
2023-07-05 22:25:40 +0300

    Delete HomeWork/Seinar5 directory

--- HomeWork/Seinar5/Task3.py
+++
@@ -1,9 +0,0 @@
-# Создайте функцию генератор чисел Фибоначчи (см. Википедию).
-def fibonaci(a):
-    a1, a2 = 1, 1
-    print(a1, a2, end=" " )
-    for i in range(a + 1):
-        yield a1+a2
-        a1, a2 = a2, a1+a2
-
-print(*fibonaci(10))

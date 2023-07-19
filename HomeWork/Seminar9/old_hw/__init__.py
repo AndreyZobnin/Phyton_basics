6ac0d5f360a163780432c14e36e49c4afacf0365
AndreyZobnin <111076368+AndreyZobnin@users.noreply.github.com>
2023-07-19 16:24:16 +0300

    Add files via upload

---
+++ HomeWork/Seminar9/old_hw/__init__.py
@@ -0,0 +1,6 @@
+from Seminar9.old_hw.hw9 import guessing_numbers
+from Seminar9.Task1 import quadratic_equation
+from Seminar9.old_hw.hw1 import guessing_game
+from Seminar9.old_hw.ex6 import guesses_dict
+
+__all__ = ['quadratic_equation', 'guessing_game', 'guesses_dict', 'guessing_numbers']
